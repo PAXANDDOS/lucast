bf23deb8b74ca8e28d6106f5eaf72ddff64d6256
Paul <40315177+PAXANDDOS@users.noreply.github.com>
2022-02-05 00:34:51 +0200

    Optimized script, simplified builder config
    - Optimized build.mjs as to
    caoxiemeihao/vite-react-electron@11d46ee8830087bd180e8ab0c0557dbf8ca70584
    - Simplified electron-builder config

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -6,31 +6,20 @@ import { build } from 'vite'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-/**
- * @type {Record<string, import('vite').InlineConfig>}
- */
-const viteConfigs = {
-	main: {
-		configFile: 'scripts/vite.config.mjs',
-		root: join(__dirname, '../packages/main'),
-		build: {
-			outDir: '../../dist/main',
-		},
+await build({
+	configFile: 'scripts/vite.config.mjs',
+	root: join(__dirname, '../packages/main'),
+	build: {
+		outDir: '../../dist/main',
 	},
-	preload: {
-		configFile: 'scripts/vite.config.mjs',
-		root: join(__dirname, '../packages/preload'),
-		build: {
-			outDir: '../../dist/preload',
-		},
-	},
-	renderer: {
-		configFile: 'packages/renderer/vite.config.ts',
-	},
-}
+})
 
-const buildElectron = async () => {
-	for (const [, config] of Object.entries(viteConfigs)) await build(config)
-}
+await build({
+	configFile: 'scripts/vite.config.mjs',
+	root: join(__dirname, '../packages/preload'),
+	build: {
+		outDir: '../../dist/preload',
+	},
+})
 
-await buildElectron()
+await build({ configFile: 'packages/renderer/vite.config.ts' })
